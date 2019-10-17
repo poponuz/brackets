cf7bff6b17c20ed738842f2c6f68d4d69f703ea9
i <ixursandov01@gmail.com>
2019-10-17 12:31:12 +0500

    feat:add solutions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const brackets = bracketsConfig.map(x => x[0] + x[1]);
+    let oldStr;
+    
+    while(oldStr != str){
+      oldStr = str;
+      brackets.forEach(bracket => {
+        str = str.replace(bracket, '');
+      });
+    }
+  
+    return str.length == 0;
+  }
